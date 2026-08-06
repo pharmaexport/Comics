2a657c80bf3c0093f169ceef463bdd8c056512a9
Gaiapp <arkopharmaexport@gmail.com>
2026-08-06 03:36:29 +0200

    Ajouter la configuration des planches et cases

---
+++ comics.js
@@ -0,0 +1,17 @@
+window.COMIC_DATA = {
+  title: "Regards au Levant",
+  volume: "Tome 1",
+  panels: [
+    /*
+      Exemple d'une case découpée dans une planche :
+      {
+        image: "images/tome-1/planche-01.jpg",
+        alt: "Description de la case pour les lecteurs d'écran",
+        crop: { x: 0, y: 0, width: 50, height: 35 }
+      }
+
+      Les valeurs x, y, width et height sont des pourcentages de la planche.
+      Sans propriété crop, l'image entière est affichée.
+    */
+  ]
+};
